87bbe89051e149ef7811f1b0ef0f1ee8899092d7
JackBettam <70541349+JackBettam@users.noreply.github.com>
2023-03-11 11:00:39 +0000

    Removal of Drawing & adding minimum energy

--- Main.py
+++ Main.py
@@ -13,14 +13,34 @@ imported_file = pd.read_csv(source_file)
 smiles_col_name = list(imported_file.columns)[SMILES_column]        #identifying SMILES column
 name_col_name = list(imported_file.columns)[molecule_name_column]   #identifying molecule names
 smiles_list = imported_file[smiles_col_name].to_list()              #Producing a list of SMILES
-name_list = imported_file[name_col_name].to_list()                #Producing a list of names
+name_list = imported_file[name_col_name].to_list()                  #Producing a list of names
 
+
+#settings for conformer generation & MMFF iterations
+ConformerNumber = 10000
+MaxIterations = 200
+
+converged_count = 0
+non_converged_count = 0
 
 for i in range(len(smiles_list)):
     mol = Chem.MolFromSmiles(smiles_list[i])
     mol_name = name_list[i]
-    #print(mol_name)
-    drawer = Draw.rdMolDraw2D.MolDraw2DCairo(300,300)
-    drawer.DrawMolecule(mol, legend=mol_name)
-    drawer.FinishDrawing()
-    drawer.WriteDrawingText(str(mol_name)+'.png')
+    print(mol_name)
+    mol_h = Chem.AddHs(mol)
+    mol_h.SetProp('Name', str(mol_name))
+    conformer_ids = AllChem.EmbedMultipleConfs(mol_h, numConfs = ConformerNumber, useBasicKnowledge = True, enforceChirality = True, numThreads=0)
+    print(len(list(conformer_ids)), ' conformers')
+    MMFF_out = AllChem.MMFFOptimizeMoleculeConfs(mol_h, maxIters = MaxIterations, numThreads = 0)
+    #Initialising energies
+    minEnergy = 999999
+    minEnergy_idx = None
+    for index, result in enumerate(MMFF_out):
+        if result[0] == 0:
+            if minEnergy > result[1]:
+                minEnergy = result[1]
+                minEnergy_idx = index
+    #print(minEnergy_idx, ' : ', minEnergy)
+    mol_h.SetProp('Minimum Energy', str(minEnergy))
+    print(mol_h.GetProp('Minimum Energy'))
+
