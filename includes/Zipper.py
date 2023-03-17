872277eaceff430bdfab797a32993af6d1f0f62c
JackBettam <70541349+JackBettam@users.noreply.github.com>
2023-03-17 21:41:23 +0000

    Zipper function working

--- includes/Zipper.py
+++ includes/Zipper.py
@@ -4,14 +4,26 @@ import zipfile
 
 #InputDir should be a directory
 
-def compress(InputDir, OutputDir = 'InputDir', FileExtension='All'):
+def compress(InputDir, OutputDir = None, FileExtension='All', ZipName = 'Data.zip'):
     AllFiles = os.listdir(InputDir)
-    if FileExtension == 'All':
+    if FileExtension == 'All':          #checks filetypes
         ZipFiles = AllFiles
     else:
         ZipFiles = []
         for i in range(len(AllFiles)):
             if AllFiles[i].split('.')[-1] == FileExtension:
                 ZipFiles.append(AllFiles[i])
+    if OutputDir == None:               #checks output dir
+        OutputDir = InputDir
+    i, v = list(enumerate(OutputDir))[-1]
+    if i != '\\':
+        OutputDir = str(OutputDir) + '\\'
+    print(OutputDir)
+    compression = zipfile.ZIP_DEFLATED
+    zf = zipfile.ZipFile(OutputDir + ZipName, mode = 'w')
+    for file in ZipFiles:
+        #print(file)
+        zf.write(InputDir + '\\' +file, file, compress_type=compression)
     
+
 compress(r'C:\Users\jackb\OneDrive\Documents\GitHub\ConformerEnergyMin\output', FileExtension = 'sdf')
