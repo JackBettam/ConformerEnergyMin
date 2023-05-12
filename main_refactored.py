06f22eda71ba0749c2e476b3ed7e2bd487d862ed
JackBettam <70541349+JackBettam@users.noreply.github.com>
2023-05-12 10:54:32 +0100

    Update main_refactored.py

--- main_refactored.py
+++ main_refactored.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from includes.Logger import logger
 from includes.ImportExport import ImportExport, TempDirGen, TempDirCleanup
+from rdkit import Chem
+from rdkit.Chem import AllChem, Draw
 
 #To do:
-    #1 MAKE readdata function
+    # DONE 1 Make readdata function
     #2 Convert mol iterator into a function
     #3 refactor the comparing minimum energy optimisations
     #4 use a with loop to close - ensure memory stability
@@ -19,9 +21,21 @@ def ReadData(InputFile):
     smiles_col_name = list(imported_file.columns)[SMILES_column]       
     name_col_name = list(imported_file.columns)[molecule_name_column]  
     smiles_list = imported_file[smiles_col_name].to_list()              
-    name_list = imported_file[name_col_name].to_list()                  
-    return name_list, smiles_list
+    name_list = imported_file[name_col_name].to_list()
+    result = zip(name_list, smiles_list)
+    logger(str(InputFile) + ' columns zipped succesfully. Returning to caller function.')                  
+    return result
 
-name_list, smiles_list = ReadData(source_file)
+data = list(ReadData(source_file))
+print(data)
 
-print(name_list)
+def GenerateConformers(ZippedData, ConformersGenerated = 10000, MaxIterations = 500):
+    for name, smile in ZippedData:
+        mol = Chem.MolFromSmiles(smile)
+        mol_h = Chem.AddHs(mol)
+        mol_h.SetProp('_Name', name)
+
+
+
+
+GenerateConformers(data)
